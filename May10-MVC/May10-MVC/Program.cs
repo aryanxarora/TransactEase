3a4dd9f1f3d09c6540d7ccbeb4d912f465153c17
Aryan Arora <2219549@edu.vaniercollege.qc.ca>
2023-05-10 10:35:11 -0400

    Initial Commit

---
+++ May10-MVC/May10-MVC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace May10_MVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World May 10");
+
+            Console.ReadKey();
+        }
+    }
+}

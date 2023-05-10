8a60890667c4c824be71e446de7c137391bb196b
Aryan Arora <2219549@edu.vaniercollege.qc.ca>
2023-05-10 10:48:02 -0400

    MVC Calculator App

---
+++ May10/MVC-Calculator/MVC-Calculator/Controller/controller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVC_Calculator.Model;
+
+namespace MVC_Calculator.Controller
+{
+    class controller
+    {
+        public model model;
+    }
+}

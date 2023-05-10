df5ffe43cd9c94714496a21b3951da4641d3fe74
Aryan Arora <2219549@edu.vaniercollege.qc.ca>
2023-05-10 11:34:54 -0400

    Abstraction & Inheritance of Shapes

---
+++ May10/Inheritance-Shapes/Inheritance-Shapes/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Shapes
+{
+    class Circle : Shape
+    {
+
+        public Circle(double length) : base(length/2, length/2)
+        {
+            Console.WriteLine("Circle Created!");
+        }
+        public override double calculateArea()
+        {
+            return Math.PI * Math.Pow(this.getWidth(), 2);
+        }
+
+        public override double calculatePerimeter()
+        {
+            return 2 * Math.PI * this.getWidth();
+        }
+    }
+}
---
+++ May10/Inheritance-Shapes/Inheritance-Shapes/Square.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Shapes
+{
+    class Square : Shape
+    {
+        public Square(double width) : base(width, width)
+        {
+            Console.WriteLine("Square Created!");
+        }
+        public override double calculateArea()
+        {
+            return Math.Pow(this.getWidth(), 2);
+        }
+
+        public override double calculatePerimeter()
+        {
+            return 4 * this.getWidth();
+        }
+    }
+}

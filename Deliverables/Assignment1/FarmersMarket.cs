178e737cf7a535d6cae4c9d51b2ca578fda7f9e5
aryanxarora <2219549@edu.vaniercollege.qc.ca>
2023-06-01 18:31:38 -0400

    ListView and Farmers Market Class Completed

---
+++ Deliverables/Assignment1/FarmersMarket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class FarmersMarket
+    {
+        // id, name, kg, price
+        private int id;
+        private string name;
+        private double weight;
+        private double price;
+
+        public FarmersMarket(int id, string name, double weight, double price)
+        {
+            this.id = id;
+            this.name = name;
+            this.weight = weight;
+            this.price = price;
+        }
+
+        public int _id { get { return id; } set { this.id = value; } }
+        public string _name { get { return name; } set { this.name = value; } }
+        public double _weight { get { return weight; } set { this.weight = value; } }
+        public double _price { get { return price; } set { this.price = value;  } }
+    }
+}
---
+++ Deliverables/Assignment1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<FarmersMarket> products = new List<FarmersMarket>();
+        public List<FarmersMarket> Products
+        {
+            get { return products;  }
+            set { products = value; }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            products.Add(new FarmersMarket(1, "Apple", 23, 2.10));
+            products.Add(new FarmersMarket(2, "Orange", 20, 2.49));
+            products.Add(new FarmersMarket(3, "Raspberry", 25, 2.35));
+            products.Add(new FarmersMarket(4, "Blueberry", 29, 1.45));
+            products.Add(new FarmersMarket(5, "Cauliflower", 24, 2.22));
+
+            DataContext = this;
+        }
+    }
+}
